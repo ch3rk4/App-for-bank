cbe008b4ce0a9dcf0a203d98eeb4f035bf5fafa0
Гребелин Матвей <matveigrebelin@yandex.ru>
2024-11-08 20:00:19 +0300

    working widget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
 
 card_date = str(input("Введите данные: "))
+date = str(input("Введите дату: "))
 
 print(mask_account_card(card_date))
+print(get_date(date))
--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,16 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(user_date: str) -> str:
+    "Func that masks user's date"
     last_space = user_date.rfind(" ")
 
     if last_space == -1:
         return "Ошибка в вводе данных"
 
-    number = user_date[last_space + 1:]
+    number = user_date[last_space + 1 :]
 
-    if "Счет" or "Счёт" in user_date:
+    if "Счет" in user_date or "Счёт" in user_date:
         if len(number) == 20:
             n_date = get_mask_account(number)
             return f"{user_date[:last_space]} {n_date}"
@@ -20,3 +22,9 @@ def mask_account_card(user_date: str) -> str:
             return f"{user_date[:last_space]} {n_date}"
         else:
             return "Ошшибка в вводе данных"
+
+
+def get_date(date: str) -> str:
+    "Func that shows date like 'DD.MM.YYYY'"
+    n_date = date[0:10].split("-")
+    return ".".join(n_date[::-1])
