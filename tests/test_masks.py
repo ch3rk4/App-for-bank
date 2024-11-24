e07b970e37a658a9a9a99025f116d32fa93561c6
ch3rk4 <matveigrebelin@yandex.ru>
2024-11-24 15:06:49 +0300

    Test funcs from masks for basic values

---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def test_get_mask_card_number_1(numbers_1):
+    assert get_mask_card_number("7000792289606361") == numbers_1
+
+
+def test_get_mask_account_1(numbers_1):
+    with pytest.raises(AssertionError):
+        assert get_mask_account("7000792289606361") == numbers_1
+
+
+def test_get_mask_card_number_2(numbers_2):
+    with pytest.raises(AssertionError):
+        assert get_mask_card_number("73654108430135874305") == numbers_2
+
+
+def test_get_mask_account_2(numbers_2):
+    assert get_mask_account("73654108430135874305") == numbers_2
